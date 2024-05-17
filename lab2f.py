63517e24b615c2e3af97db010f931f7fb853771a
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-17 16:52:27 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#author: Ali Teimouri
+#author ID: ateimouri1
+#Date Created 2024/05/17
+
+
+import sys
+
+# give the timer object's value of int(sys.argv[1]).
+timer = int(sys.argv[1])
+
+# while loop that keeps going until timer equals 0
+while timer !=0:
+    print(timer)
+    timer -= 1
+print("blast off!") 
