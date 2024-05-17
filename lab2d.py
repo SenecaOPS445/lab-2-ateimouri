8551ad972a8e7567e913a2e146d392614b291140
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-17 16:34:58 -0400

    added lab2d

---
+++ lab2d.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+#args = len(sys.argv)
+# verify how many arguments there are calling the file; one argument counts.
+# If there aren't precisely two more parameters check for three since calling the file with "./lab2d.py" counts as one argument, print a usage message.
+
+if len(sys.argv) != 3:
+    print('Usage: ./lab2d.py name age')
+    sys.exit()
+
+# Make variables to hold argumts string objects are used to store arguments.
+name = sys.argv[1]
+age = sys.argv[2]
+
+# Print out
+print('Hi ' + name + ', you are ' + age + ' years old.')
+
+
