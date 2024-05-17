3c0b3db1d9f7c94774c46f81447878c136ecfa94
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-17 16:44:43 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#author: Ali Teimouri
+#author ID: ateimouri1
+#Date Created 2024/05/17
+
+'''created a countdown with while loop '''
+timer = 10
+while timer !=0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+    
