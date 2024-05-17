1d8eff073551b14554c6794b77935c9a2a4c0760
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-17 17:00:47 -0400

    lab2d.py

--- lab2d.py
+++ lab2d.py
@@ -3,18 +3,18 @@
 import sys
 
 #args = len(sys.argv)
-# verify how many arguments there are calling the file; one argument counts.
-# If there aren't precisely two more parameters check for three since calling the file with "./lab2d.py" counts as one argument, print a usage message.
+#verify how many arguments there are calling the file; one argument counts.
+#if there aren't precisely two more parameters check for three since calling the file with "./lab2d.py" counts as one argument, print a usage message.
 
 if len(sys.argv) != 3:
     print('Usage: ./lab2d.py name age')
     sys.exit()
 
-# Make variables to hold argumts string objects are used to store arguments.
+#make variables to hold argumts string objects are used to store arguments.
 name = sys.argv[1]
 age = sys.argv[2]
 
-# Print out
+#print out
 print('Hi ' + name + ', you are ' + age + ' years old.')
 
 
