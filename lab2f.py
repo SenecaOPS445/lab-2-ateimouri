9003315eaf540c612d85728721d5617ce1e34e13
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-17 16:59:34 -0400

    lab2f.py

--- lab2f.py
+++ lab2f.py
@@ -7,10 +7,10 @@
 
 import sys
 
-# give the timer object's value of int(sys.argv[1]).
+#give the timer object's value of int(sys.argv[1]).
 timer = int(sys.argv[1])
 
-# while loop that keeps going until timer equals 0
+#while loop that keeps going until timer equals 0
 while timer !=0:
     print(timer)
     timer -= 1
